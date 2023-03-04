828171e6e2a51b5bc9461a6620119598be909800
bhsh200 <117628732+bhsh200@users.noreply.github.com>
2023-03-04 20:43:30 +0530

    Add files via upload

---
+++ notes/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+
+
+class Note(models.Model):
+    author = models.TextField()
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    isPin = models.BooleanField(default=False)
+    id = models.AutoField(primary_key=True)
+    primary = models.CharField(max_length=20, null=True)
+    secondary = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.title
